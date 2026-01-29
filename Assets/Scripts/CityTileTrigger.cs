4d500a7dab9983299f32e8ed2ec6d12180f4b566
princekr77 <princekr2927@gmail.com>
2026-01-30 00:22:43 +0530

    commit

--- Assets/Scripts/CityTileTrigger.cs
+++ Assets/Scripts/CityTileTrigger.cs
@@ -2,27 +2,18 @@
 
 public class CityTileTrigger : MonoBehaviour
 {
-    private bool triggered = false;
     private GameObject parentTile;
 
     void Start()
     {
-        // This trigger belongs to this tile
         parentTile = transform.root.gameObject;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !triggered)
+        if (other.CompareTag("Player"))
         {
-            triggered = true;
             CityTileSpawner.Instance.OnPlayerHitTrigger(parentTile);
         }
-    }
-
-    // Called when tile is reused or moved
-    public void ResetTrigger()
-    {
-        triggered = false;
     }
 }
